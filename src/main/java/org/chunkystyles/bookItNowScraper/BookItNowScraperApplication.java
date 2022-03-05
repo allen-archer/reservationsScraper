00ce2aa0a98994042d6c6d593876d04411ff481e
Allen <chunkystyles@gmail.com>
2022-03-05 14:31:48 -0500

    Initial code from spring intializr

---
+++ src/main/java/org/chunkystyles/bookItNowScraper/BookItNowScraperApplication.java
@@ -0,0 +1,13 @@
+package org.chunkystyles.bookItNowScraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookItNowScraperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookItNowScraperApplication.class, args);
+	}
+
+}
