ff291046e5916f75230466068c24b44270479cef
Allen <chunkystyles@gmail.com>
2022-05-27 09:23:38 -0400

    Added file existence checks and retries to snapshot uploads.

--- node_project/frigate.js
+++ node_project/frigate.js
@@ -1,4 +1,5 @@
 const {Webhook} = require("discord-webhook-node");
+const fs = require('fs');
 
 let config
 let secrets
@@ -19,9 +20,28 @@ async function sendMessage(message){
 }
 
 async function sendFile(file){
+    let count = 0
+    let success = false
+    do {
+        if (!fs.existsSync(file)){
+            logger.error('File ' + file + ' does not exist yet.  Sleeping for ' + config.snapshot.sleep + ' seconds.')
+            count++
+            await wait(config.snapshot.sleep * 1000)
+        } else {
+            success = true
+        }
+    } while (count < config.snapshot.retries && !success)
+    if (!success){
+        logger.error('File ' + file + ' does not exist')
+        return
+    } else {
+        logger.info('File ' + file + ' exists, sending file')
+    }
     webhook
         .sendFile(file)
         .catch(e => logger.error(e.message))
 }
+
+const wait = (ms) => new Promise(res => setTimeout(res, ms))
 
 module.exports = { initialize, sendMessage, sendFile }
